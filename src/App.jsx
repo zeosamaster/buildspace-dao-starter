89f0a6be79557a7dff53e54239bf40db59c6f33f
Fábio Matias <fabio_mcm2@hotmail.com>
2021-12-23 01:01:52 +0000

    section-1: connect to wallet

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
+import React from "react";
+import { useWeb3 } from "@3rdweb/hooks";
+
 const App = () => {
+  const { connectWallet, address } = useWeb3();
+
+  React.useEffect(() => {
+    console.log("👋 Address:", address);
+  }, [address]);
+
+  if (!address) {
+    return (
+      <div className="landing">
+        <h1>Welcome to FootyDAO</h1>
+        <button onClick={() => connectWallet("injected")} className="btn-hero">
+          Connect your wallet
+        </button>
+      </div>
+    );
+  }
+
   return (
     <div className="landing">
-      <h1>Welcome to My DAO</h1>
+      <h1>👀 wallet connected, now what!</h1>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ThirdwebWeb3Provider } from "@3rdweb/hooks";
 import "./index.css";
-import App from "./App.jsx";
+import App from "./App";
+
+const supportedChainIds = [4];
+
+const connectors = {
+  injected: {},
+};
 
 // Render the App component to the DOM
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThirdwebWeb3Provider
+      connectors={connectors}
+      supportedChainIds={supportedChainIds}
+    >
+      <div className="landing">
+        <App />
+      </div>
+    </ThirdwebWeb3Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
