d0e1b3fe6363183e5995f71370f9859a5afaa8be
Fábio Matias <fabio_mcm2@hotmail.com>
2021-12-25 22:40:38 +0000

    section-3: fetch token holders and balances

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { ThirdwebSDK } from "@3rdweb/sdk";
 import { useWeb3 } from "@3rdweb/hooks";
+import { ethers } from "ethers";
 
 const sdk = new ThirdwebSDK("rinkeby");
 
@@ -8,10 +9,21 @@ const bundleDropModule = sdk.getBundleDropModule(
   "0x4C63B7D8933c88d2E924141c8EC3951A2E395A48"
 );
 
+const tokenModule = sdk.getTokenModule(
+  "0x16F5F60FB05E965382Bf19ee55982E3B7984b69a"
+);
+
+const shortenAddress = (str) => {
+  return str.substring(0, 6) + "..." + str.substring(str.length - 4);
+};
+
 const App = () => {
   const { connectWallet, address, error, provider } = useWeb3();
   const [hasClaimedNFT, setHasClaimedNFT] = React.useState(false);
   const [isClaiming, setIsClaiming] = React.useState(false);
+
+  const [memberTokenAmounts, setMemberTokenAmounts] = React.useState({});
+  const [memberAddresses, setMemberAddresses] = React.useState([]);
 
   const signer = provider ? provider.getSigner() : undefined;
 
@@ -59,6 +71,56 @@ const App = () => {
 
     setIsClaiming(false);
   }, []);
+
+  React.useEffect(() => {
+    if (!hasClaimedNFT) {
+      return;
+    }
+
+    const getMemberAddresses = async () => {
+      try {
+        const addresses = await bundleDropModule.getAllClaimerAddresses("0");
+        console.log("🚀 Member addresses", addresses);
+        setMemberAddresses(addresses);
+      } catch (e) {
+        setMemberAddresses([]);
+        console.error("failed to get member list", e);
+      }
+    };
+
+    getMemberAddresses();
+  }, [hasClaimedNFT]);
+
+  React.useEffect(() => {
+    if (!hasClaimedNFT) {
+      return;
+    }
+
+    const fetchTokenBalances = async () => {
+      try {
+        const amounts = await tokenModule.getAllHolderBalances();
+        console.log("👜 Amounts", amounts);
+        setMemberTokenAmounts(amounts);
+      } catch (e) {
+        setMemberTokenAmounts({});
+        console.error("failed to get token amounts", e);
+      }
+    };
+
+    fetchTokenBalances();
+  }, [hasClaimedNFT]);
+
+  const memberList = React.useMemo(() => {
+    return memberAddresses.map((address) => {
+      return {
+        address,
+        tokenAmount: ethers.utils.formatUnits(
+          memberTokenAmounts[address] || 0,
+          18
+        ),
+      };
+    });
+  }, [memberAddresses, memberTokenAmounts]);
 
   if (!address) {
     return (
